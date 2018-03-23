645ceb54ea00ce2de32e024940033640a86349f6
Natalia <natalia.voblikova@gmail.com>
2018-03-23 11:57:12 +0300

    1я лекция

---
+++ java-homework/src/lecture2/loops/Loops.java
@@ -0,0 +1,141 @@
+package lecture2.loops;
+
+
+public class Loops {
+    public static void main(String[] args) {
+        // print4DigitSequence();
+        //printElementsMultiplesOf5();
+        //printElementsMultiplesOf2();
+
+        checkPrimeNumber(89);
+        countLuckyTicket();
+        countSymmetricalCombinations();
+    }
+
+    /**
+     * 1) Создайте программу, выводящую на экран все четырёхзначные числа
+     * последовательности 1000 1003 1006 1009 1012 1015 …
+     */
+
+//    public static void print4DigitSequence(){
+//        int num = 1000;
+//        while (num < 10000){
+//            System.out.println(num);
+//            num+=3;
+//        }
+//    }
+
+    /**
+     * 2) Создайте программу, выводящую на экран все неотрицательные элементы
+     * последовательности 90 85 80 75 70 65 60 …
+     */
+
+//    public static void printElementsMultiplesOf5(){
+//        for (int num = 90; num > 0; num--){
+//            if (num%5 == 0) {
+//                System.out.println(num);
+//            }
+//        }
+//    }
+
+    /**
+     * 3) Создайте программу, выводящую на экран первые 20 элементов
+     * последовательности 2 4 8 16 32 64 128 …
+     */
+
+//    public static void printElementsMultiplesOf2() {
+//        int count = 0;
+//        int num = 2;
+//        while (count < 20){
+//            System.out.println(num);
+//            count++;
+//            num *= 2;
+//        }
+//    }
+
+    /**
+     * 4) Проверьте,  является ли  натуральное число — простым.
+     */
+        // не решена до конца
+    public static void checkPrimeNumber(int num) {
+        int k;
+        String res = "Простое число";
+        if (num % 2 != 0 && num != 2) {
+            k = (int) Math.ceil(Math.sqrt(num));
+            for (int i = 3; i < k; i++) {
+                if (num % i == 0) {
+                    System.out.println(res);
+                } else {
+                    System.out.println("Не является ростое число");
+                }
+            }
+        } else {
+            System.out.println(res);
+        }
+    }
+
+
+    /**
+     * 5) Выведите на экран первые 11 членов последовательности Фибоначчи.
+     * Первый и второй члены последовательности равны единицам, а каждый
+     * следующий — сумме двух предыдущих.
+     */
+
+    public static void k(){
+
+    }
+
+    /**
+     * 6) В городе N проезд в трамвае осуществляется по бумажным отрывным билетам.
+     * Каждую неделю трамвайное депо заказывает в местной типографии рулон билетов
+     * с номерами от 000001 до 999999. «Счастливым» считается билетик у которого
+     * сумма первых трёх цифр номера равна сумме последних трёх цифр, как, например,
+     * в билетах с номерами 003102 или 567576. Трамвайное депо решило подарить сувенир
+     * обладателю каждого счастливого билета и теперь раздумывает, как много сувениров
+     * потребуется. С помощью программы подсчитайте сколько счастливых билетов в одном рулоне?
+     */
+
+    // не решена
+    public static void countLuckyTicket() {
+        int iSum;
+        int jSum;
+        int count = 0;
+        for (int i =0; i < 999; i++){
+            for (int j = 1; j <999; j++){
+               iSum = i%100/10 + i%100%10 +i%100;
+               jSum = j%100/10 + j%100%10 +j%100;
+               if (i == j){
+                   count++;
+               }
+            }
+        }
+        System.out.println(count);
+
+    }
+
+    /**
+     * 7) Электронные часы показывают время в формате от 00:00 до 23:59. Подсчитать
+     * сколько раз за сутки случается так, что слева от двоеточия показывается симметричная
+     * комбинация для той, что справа от двоеточия (например, 02:20, 11:11 или 15:51).
+     */
+
+    public static void countSymmetricalCombinations(){
+        int h;
+        int m;
+        String tmpH;
+        String tmpM;
+        int count = 0;
+
+        for(h =0; h!=24; h++ ){
+            for(m=0; m!=60; m++) {
+                //0 - подстановка, 2 - разрядность, d - целое число
+                tmpH = String.format("%02d", h);
+                tmpM = String.format("%02d", m);
+                if (tmpH.charAt(0) == tmpM.charAt(1) && tmpH.charAt(1) == tmpM.charAt(0)) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ java-homework/src/lecture2/loops/t2.java
@@ -0,0 +1,124 @@
+package lecture2.loops;
+
+//
+public class t2 {
+    public static void main(String[] args) {
+       minDistance(8.5, 11.45);
+       processQuadraticEquation(1, 7, 9);
+       maxDigit(774);
+       sortAscending(3, 9, -1);
+    }
+
+    /**
+     * 1) Создать программу, выводящую на экран ближайшее к 10 из двух чисел,
+     * записанных в переменные m и n. Например, среди чисел 8.5 и 11.45
+     * ближайшее к десяти 11.45.
+     */
+
+    public static void minDistance(double m, double n){
+        double result;
+        // Math.abs(n) — возвращает модуль числа n.
+        if (Math.abs(10 - n) < Math.abs(10 - m)){
+            result = n;
+        } else {
+            result = m;
+        }
+        System.out.println(result);
+    }
+
+    /**
+     * 2) В три переменные a, b и c записаны три вещественных числа.
+     * Создать программу, которая будет находить и выводить на экран вещественные
+     * корни квадратного уравнения ax²+bx+c=0, либо сообщать, что корней нет.
+     */
+
+    public static void processQuadraticEquation(double a, double b, double c) {
+        //дискриминант —  D = b*b − 4ac.
+        double d = b * b - 4 * a * c;
+        double x1, x2;
+        if (d > 0) {
+            x1 = (-b + Math.sqrt(d)) / (2 * a);
+            x2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println(String.format("%f, %f", x1, x2));
+        } else if (d == 0){
+            x1 = (-b + Math.sqrt(d)) / (2 *a);
+            System.out.println(x1);
+        } else {
+            System.out.println("Корней нет");
+        }
+    }
+
+    /**
+     * 3) Создать программу, выводящую на экран наибольшую цифру любого
+     * трехзначного натурального числа. Примеры работы программы:
+     *
+     * В числе 208 наибольшая цифра 8
+     * В числе 774 наибольшая цифра 7
+     * В числе 613 наибольшая цифра 6
+     */
+
+    public static void maxDigit(int number) {
+//        String num = String.valueOf(number);
+//        int maxDigit = Integer.parseInt(String.valueOf(num.charAt(0)));
+//        for(int i = 1; i < num.length(); i++){
+//            int currentDigit = Integer.parseInt(String.valueOf(num.charAt(i)));
+//            if (maxDigit < currentDigit){
+//                maxDigit = currentDigit;
+//            }
+//        }
+
+        int maxDigit = 0;
+        int currentNumber = number;
+        do {
+            int currentDigit = currentNumber % 10;
+            if (currentDigit > maxDigit) {
+                maxDigit = currentDigit;
+            }
+            currentNumber = currentNumber / 10;
+        } while (currentNumber > 0);
+
+        System.out.println("В числе " + number + " наибольшая цифра " + maxDigit);
+    }
+
+    /**
+     * 4) В три переменные a, b и c явно записаны программистом три целых
+     * попарно неравных между собой числа. Создать программу, которая переставит
+     * числа в переменных таким образом, чтобы при выводе на экран последовательность
+     * a, b и c оказалась строго возрастающей.
+     *
+     * Числа в переменных a, b и c: 3, 9, -1
+     * Возрастающая последовательность: -1, 3, 9
+     *
+     * Числа в переменных a, b и c: 2, 4, 3
+     * Возрастающая последовательность: 2, 3, 4
+     *
+     * Числа в переменных a, b и c: 7, 0, -5
+     * Возрастающая последовательность: -5, 0, 7
+     */
+
+    public static void sortAscending(int a, int b, int c) {
+        String pattern = "%d, %d, %d";
+        String result;
+        if (a < b && a < c){
+            if(b < c){
+                result = String.format(pattern, a, b, c);
+            } else {
+                result = String.format(pattern, a, c, b);
+            }
+        } else if (b < c && b < a) {
+            if (c < a) {
+                result = String.format(pattern, b, c, a);
+            } else {
+                result = String.format(pattern, b, a, c);
+            }
+        } else {
+            if (a < b) {
+                result = String.format(pattern, c, a, b);
+            } else {
+                result = String.format(pattern, c, b, a);
+            }
+        }
+        System.out.println(result);
+    }
+
+}
